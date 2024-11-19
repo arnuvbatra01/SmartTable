d73244cb57223f27fef28d5ad9e4a76982d1ca34
arnuvbatra01 <80216186+arnuvbatra01@users.noreply.github.com>
2024-11-18 22:16:56 -0500

    Add files via upload

--- Foosball.ino
+++ Foosball.ino
@@ -1,9 +1,10 @@
 #include <Adafruit_Fingerprint.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
-#include wire.h
-#include LCD.h
-#include LiquidCrystal_I2C.h
-#include pitches.h
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include "pitches.h"
 
 #define I2C_ADDR 0x27 //Define I2C Address where the PCF8574A is
 #define BACKLIGHT_PIN 3
@@ -20,25 +21,35 @@
 int player1_wins = 0;
 int player2_wins = 0;
 
+int p1_score = 0;
+int p2_score = 0;
+
 SoftwareSerial mySerial(2, 3);
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 int fingerprintIDs [50];
 int prints = 0;
-LiquidCrystal_I2C lcd(I2C_ADDR, En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
+LiquidCrystal_I2C lcd(0x27,16,2);
 
 void setup () {
 
-lcd.begin (16,2);
-lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
-lcd.setBacklight(HIGH);
-lcd.print("Welcome to SmartTable Foosball");
-delay(1000);
+  lcd.begin();
+  lcd.setCursor(0,0);
+  lcd.print("Welcome to Smart");
+  lcd.setCursor(0,1);
+  lcd.print("Table Foosball!");
+  delay(3000);
+  lcd.clear();
 
     int temp_prints = prints;
     while (temp_prints == prints) {
-lcd.print("Player 1. Scan your finger");
-fingerprintID = getFingerprintID();
-delay (50);
+lcd.clear();
+lcd.setCursor(0,0);
+lcd.print("Player 1.");
+lcd.setCursor (0,1);
+lcd.print("Scan your finger");
+//fingerprintID = getFingerprintID();
+int fingerprintID = 1;
+delay (1000);
 
 if (fingerprintID == 1){
     fingerprintIDs[prints] = 1;
@@ -53,9 +64,13 @@ else if (fingerprintID == 2){
     temp_prints = prints;
     lcd.clear();
     while (temp_prints == prints) {
-lcd.print("Player 2. Scan your finger");
-fingerprintID = getFingerprintID();
-delay (50);
+lcd.setCursor(0,0);
+lcd.print("Player 2");
+lcd.setCursor (0,1);
+lcd.print("Scan your finger");
+//fingerprintID = getFingerprintID();
+int fingerprintID = 2;
+delay (1000);
 
 if (fingerprintID == 1){
     fingerprintIDs[prints] = 1;
@@ -69,48 +84,61 @@ else if (fingerprintID == 2){
 
 //Initialize Game: 
 
-int p1_score = 0;
-int p2_score = 0;
-lcd.print("Starting game.");
+lcd.clear();
+lcd.setCursor(0,0);
+lcd.print("Starting game...");
+make_sound();
 delay(1000);
+lcd.clear();
 
 }
 
 void loop () {
 
+  lcd.setCursor(0,0);
   lcd.print ("Player 1: " + String(p1_score));
   lcd.setCursor (0, 1);
   lcd.print ("Player 2: " + String (p2_score));
 
-  int game_update = (!play_game(p1_score, p2_score));
+  int game_update = (play_game(p1_score, p2_score));
 
   if (game_update == 1) {
     p1_score++;
     make_sound ();
-    cloud_game_score(p1_score, p2_score);
+    //cloud_game_score(p1_score, p2_score);
   }
   else if (game_update == 2){
     p2_score++;
     make_sound ();
-    cloud_game_score(p1_score, p2_score);
+    //cloud_game_score(p1_score, p2_score);
   }
 
   if (p1_score == 10){
     win_sound();
     lcd.clear();
+    lcd.setCursor(0,0);
     lcd.print("Player 1 Wins!");
+    delay(5000);
     player1_wins++;
-    cloud_analytics (player1_wins, player2_wins);
+    //cloud_analytics (player1_wins, player2_wins);
     p1_score = 0;
     p2_score = 0;
+    lcd.clear();
+    delay(2000);
 
   }
     else if (p2_score == 10){
     win_sound();
     lcd.clear();
+    lcd.setCursor(0,0);
     lcd.print("Player 2 Wins!");
+    delay(5000);
     player2_wins++;
-    cloud_analytics (player1_wins, player2_wins);
+    //cloud_analytics (player1_wins, player2_wins);
+    p1_score = 0;
+    p2_score = 0;
+    lcd.clear();
+    delay(2000);
 
   }
 
@@ -139,7 +167,9 @@ void win_sound () {
   noTone(8);
 }
 
+
 // Needs to be written to upload analytics to cloud
+/*
 void cloud_analytics (int p1, int p2){
   return void;
 }
@@ -148,8 +178,8 @@ void cloud_analytics (int p1, int p2){
 void cloud_game_score (int p1, int p2){
   return void;
 }
-
+ */
 // Needs to be written with a while loop waiting for beam to be broken
 int play_game (int p1, int p2) {
-  return 0;
+  return 1;
 }
